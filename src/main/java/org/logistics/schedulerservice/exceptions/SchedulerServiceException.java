79bcf00038a01f0b306febf6888ee45a8205ce03
anurag0510 <anurag0510@outlook.com>
2022-12-03 19:43:34 +0530

    defined exceptions for scheduler

---
+++ src/main/java/org/logistics/schedulerservice/exceptions/SchedulerServiceException.java
@@ -0,0 +1,8 @@
+package org.logistics.schedulerservice.exceptions;
+
+public class SchedulerServiceException extends RuntimeException {
+
+    public SchedulerServiceException(String message) {
+        super(message);
+    }
+}
